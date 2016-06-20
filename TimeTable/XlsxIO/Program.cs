14206b9b1a7765997a56f94e254bc3fc0fa48bd5
RomanDubinin <roman230394@yandex.ru>
2016-06-21 03:51:51 +0500

    Сдвиг дат

--- TimeTable/XlsxIO/Program.cs
+++ TimeTable/XlsxIO/Program.cs
@@ -30,7 +30,8 @@ namespace XlsxIO
 			{
 				
 			}
-			DropNearestDay(book, sheetName);
+			ShiftDays(book, sheetName);
+			ShiftDates(book, sheetName);
 
 			var str = sheet.Row(4).Cell(33).Value.ToString().Split(',').ToList()[0];
 
@@ -40,13 +41,35 @@ namespace XlsxIO
 
 		}
 
-		public static void DropNearestDay(ClosedXML.Excel.XLWorkbook book, string sheetName)
+		public static void ShiftDays(XLWorkbook book, string sheetName)
 		{
 			var sheet = book.Worksheets.Worksheet(sheetName);
 
 			for (int i = FirstStringNum; i < FirstStringNum + WorkersCount; i++)
 				for (int j = FirstColumnNum; j < FirstColumnNum + DaysCount; j++)
 					sheet.Row(i).Cell(j).Value = sheet.Row(i).Cell(j + 1);
+
+			book.Save();
+		}
+
+		public static void ShiftDates(XLWorkbook book, string sheetName)
+		{
+			var sheet = book.Worksheets.Worksheet(sheetName);
+
+			var dayNum =	int.Parse(sheet.Row(FirstStringNum - 1).Cell(FirstColumnNum + DaysCount - 1).Value.ToString());
+			var monthNum =	int.Parse(sheet.Row(FirstStringNum - 2).Cell(FirstColumnNum + DaysCount - 1).Value.ToString());
+			var yearNum =	int.Parse(sheet.Row(1).Cell(1).Value.ToString());
+
+			var lastDate  = new DateTime(yearNum, monthNum, dayNum);
+			var newLastDate = lastDate.AddDays(1);
+
+			for (int i = FirstStringNum - 2; i < FirstStringNum; i++)
+				for (int j = FirstColumnNum; j < FirstColumnNum + DaysCount; j++)
+					sheet.Row(i).Cell(j).Value = sheet.Row(i).Cell(j + 1);
+
+			sheet.Row(FirstStringNum - 1).Cell(FirstColumnNum + DaysCount - 1).Value = newLastDate.Day;
+			sheet.Row(FirstStringNum - 2).Cell(FirstColumnNum + DaysCount - 1).Value = newLastDate.Month;
+			sheet.Row(1).Cell(1).Value = newLastDate.Year;
 
 			book.Save();
 		}
